2c197609324d3c745b07a8d93c05037f680f973b
deekshithasudheendra <deekshitha.sudheendra@gmail.com>
2026-06-18 20:11:53 +0530

    Delete CodeAlpha_projectName/secure coding task 3.py

--- CodeAlpha_projectName/secure coding task 3.py
+++
@@ -1,7 +0,0 @@
-username = input("Enter Username: ")
-password = input("Enter Password: ")
-
-query = "SELECT * FROM users WHERE username='" + username + "' AND password='" + password + "'"
-
-print("\nGenerated SQL Query:")
-print(query)
